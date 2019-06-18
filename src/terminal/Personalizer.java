ada3a0b8f7726ebbff3ae1ba9c75d1b049f150c8
Flip <itsvizu@gmail.com>
2019-06-18 13:29:20 +0200

    More userinformation and pin is requested and user info is stored.

--- src/terminal/Personalizer.java
+++ src/terminal/Personalizer.java
@@ -47,9 +47,24 @@ public class Personalizer {
 
 			Util.sendSelect(card);
 			
-			int pin = 1234;
+			Scanner in = new Scanner(System.in);
+			System.out.println("Please enter your 4 digit pincode:");
+			int pin = in.nextInt();
+			while(pin < 1000 | pin >9999) {
+				System.out.println("Pin entered incorrectly, please try again");
+			}
+			System.out.println("Please enter your name:");
+			String name = in.nextLine();
+			
+			System.out.println("Please enter your BSN:");
+			BigInteger bsn = in.nextBigInteger();
+			in.close();
+			
+			
+			
+			
 			int cardNumber = new Random().nextInt();
-			BackEnd.getInstance().registerCard(cardNumber, new Account()); //TODO remove this and uncomment earlier code
+			BackEnd.getInstance().registerCard(cardNumber, new Account(name, bsn));
 			
 			KeyPairGenerator generator;
 			try {
